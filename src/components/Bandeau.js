1e6c3006d5b7d5f0aa7f06aa8a9df819437f0c21
THIRION <thirion.margaux@gmail.com>
2023-11-16 16:58:24 +0100

    update bandeau

--- src/components/Bandeau.js
+++ src/components/Bandeau.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import "../styles/bandeau.css"
 import LogoImage from '../Logo/logo.png';
-import NomAssoImage from '../Logo/NomAsso.png';
+import NomAssoImage from '../Logo/NomAssoGris.png';
 
 function Bandeau() {
   return (
     <div className="Bandeau">
-      <div className="Fond" />
-      <img className="Logo" src={LogoImage} alt="Logo"></img>
-      <img className="NomAsso" src={NomAssoImage} alt="NomAsso"></img>
+      <div className='Fond' />
+      <img className="Logo" src={LogoImage} alt="Logo" />
+      <img className="NomAsso" src={NomAssoImage} alt="NomAsso" />
     </div>
     
 
