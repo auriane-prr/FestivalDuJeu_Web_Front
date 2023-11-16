37f6b590b917fe64b1bad561c09ddedb8fdd917d
THIRION <thirion.margaux@gmail.com>
2023-11-16 17:27:00 +0100

    bandeau figé

--- src/components/bandeau.js
+++ src/components/bandeau.js
@@ -6,7 +6,6 @@ import NomAssoImage from '../Logo/NomAssoGris.png';
 function Bandeau() {
   return (
     <div className="Bandeau">
-      <div className='Fond' />
       <img className="Logo" src={LogoImage} alt="Logo" />
       <img className="NomAsso" src={NomAssoImage} alt="NomAsso" />
     </div>
