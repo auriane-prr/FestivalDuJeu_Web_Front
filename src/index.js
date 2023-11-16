a7e58cfa18dd7b35d3da31e2ab0253a5b00e7f8e
THIRION <thirion.margaux@gmail.com>
2023-11-16 18:08:08 +0100

    debut de page de login

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import Bandeau from './components/bandeau';
+import Boite from './components/boite';
+import Bouton from './components/bouton';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Bandeau />
+    <div className='Container'>
+      <Boite />
+      <div className='Bouton-Container'>
+        <Bouton>Je me connecte</Bouton>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
