70653c21a0371f2d354cb0603792708307727329
Auriane <aurianepoirier@MacBook-Pro-de-Auriane.local>
2023-11-18 11:46:34 +0100

    champ moche

--- src/components/boite.js
+++ src/components/boite.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "../styles/boite.css"
 import BoutonConnexion from './bouton';
+import Pseudo from './champ';
+import MotDePasse from './champ';
 
 function Boite() {
   return (
@@ -10,9 +12,10 @@ function Boite() {
           <div className="Couleur2"  />
           <div className="Couleur3" />
         </div> 
+        <Pseudo/>
         <BoutonConnexion>Connexion</BoutonConnexion>
       </div>
-
+    
   );
 }
 
---
+++ src/components/champ.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../styles/bouton.css"
+
+const Champ = () => {
+    return (
+      <div className="ComponentPseudo">
+        <div className="Property1Default">
+          <div className="Pseudo">Pseudo :</div>
+          <div className="SaisiePseudo" />
+        </div>
+      </div>
+    );
+  };
+
+export default Champ;
