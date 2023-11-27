69243c26101e9d2fe865f30970199834632fc98a
Auriane <aurianepoirier@macbook-pro-de-auriane.home>
2023-11-27 19:20:01 +0100

    tout début page inscription

--- src/pages/PageRegister.js
+++ src/pages/PageRegister.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Bandeau from '../components/bandeau';
 import "../styles/Pages/pageRegister.css"
+import Boite from '../components/boite';
 
 function PageRegister() {
   return (
     <div className="accueil">
       <Bandeau />
-      <div className="Titre">
-        <h1>Register</h1>
-      </div>
+      <Boite valeurDuTitre='Merci de vous joindre à nous pour cette nouvelle édition du Festival du Jeu à Montpellier !'/>
     </div>
   );
 }
