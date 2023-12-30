7506522112d3d1ce5ea7a4fafd8a818e7b8dcfd5
MargauxThirion <thirion.margaux@gmail.com>
2023-12-30 14:04:53 +0100

    changement form_stand

--- src/components/admin/form_stand.js
+++ src/components/admin/form_stand.js
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Champ from '../champ';
 
 function StandForm({ onClose }) {
   const [nom_stand, setNom_stand] = useState('');
   const [description, setDescription] = useState('');
+  const [dateDebut, setDateDebut] = useState('');
+  const [dateFin, setDateFin] = useState('');
+  const [selectedDate, setSelectedDate] = useState(''); // Initialisez avec une chaîne vide ou une valeur par défaut
+
+
   const [horairesData, setHorairesData] = useState([
     { heure: '9-11', nb_benevole: '' },
     { heure: '11-13', nb_benevole: '' },
     { heure: '13-15', nb_benevole: '' },
     { heure: '15-17', nb_benevole: '' },
-    { heure: '17-19', nb_benevole: '' },
+    { heure: '17-19', nb_benevole: '' }
   ]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    console.log("Selected Date at Submit:", selectedDate);
     if (nom_stand && horairesData.some((item) => item.nb_benevole !== '')) {
       try {
         const newStand = {
           nom_stand,
           description,
+          date: selectedDate,
           horaireCota: horairesData,
         };
+        console.log(newStand);
 
         const response = await fetch('http://localhost:3500/stands', {
           method: 'POST',
@@ -64,9 +71,26 @@ function StandForm({ onClose }) {
     onClose();
   };
 
+  //cherche les dates 
+  useEffect(() => {
+    // Exemple d'appel API pour récupérer les données du festival
+    const fetchData = async () => {
+      const result = await fetch('http://localhost:3500/festival/latest');
+      const body = await result.json();
+      setDateDebut(body.date_debut);
+      setDateFin(body.date_fin);
+      console.log(body.date_debut, body.date_fin);
+    };
+    fetchData();
+  }, []);
+  
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
+      <button type="button" onClick={() => setSelectedDate(dateDebut)}>Pour Date de Début </button>
+      <button type="button" onClick={() => setSelectedDate(dateFin)}> Pour Date de Fin</button>
+
       <Champ label='Nom Stand :'>
           <input
             className='customInput'
