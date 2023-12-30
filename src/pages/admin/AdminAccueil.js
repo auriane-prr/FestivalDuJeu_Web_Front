1739a3a29c9485aac00a6559d1222a50e646a93b
MargauxThirion <thirion.margaux@gmail.com>
2023-12-30 18:52:20 +0100

    ajout des jeux ok

--- src/pages/admin/AdminAccueil.js
+++ src/pages/admin/AdminAccueil.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BandeauAdmin from '../../components/admin/bandeauAdmin';
 import BoiteOnglet from '../../components/general/boiteOnglet';
 import Display_stand from '../../components/admin/stands/display_stand';
+import Ajouter_jeux from '../../components/admin/stands/ajouter_jeux';
 import '../../styles/Pages/admin/accueil_admin.css';
 
 function AdminPageStand() {
@@ -12,7 +13,9 @@ function AdminPageStand() {
         <div className='Onglet1'>
           <Display_stand />
         </div>
-        <div>Jeux</div>
+        <div>Jeux
+        <Ajouter_jeux />
+        </div>
       </BoiteOnglet>
     </div>
   );
