af99bd7bdb092926ba136b94890a14b55438c85f
Auriane Poirier <aurianepoirier@MacBook-Pro-de-Auriane.local>
2023-12-31 11:37:35 +0100

    details peu important

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthWrapper from './AuthWrapper';
 import FormInscription from './components/login&register/form_register';
+import './styles/App.css';
 
 function App() {
   return (
+    <div className='container'>
     <BrowserRouter>
       <Routes>
         <Route path="/inscription" element={<FormInscription />} />
         <Route path="/*" element={<AuthWrapper />} />
       </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
