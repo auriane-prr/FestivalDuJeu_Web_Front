9e570ffe74328bff3ec59542df2f9f4d27aa44ff
MargauxThirion <thirion.margaux@gmail.com>
2024-01-07 14:58:53 +0100

    admin

--- src/pages/admin/AdminAccueil.js
+++ src/pages/admin/AdminAccueil.js
@@ -6,18 +6,7 @@ function AdminAccueil() {
   return (
     <div>
       <BandeauAdmin />
-<<<<<<< HEAD
-      <BoiteOnglet nomOnglet1='Stand' nomOnglet2='Jeux'>
-        <div className='Onglet1'>
-          <Display_stand />
-        </div>
-        <div>Jeux
-        <Display_jeux />
-        </div>
-      </BoiteOnglet>
-=======
       <DisplayFestival />
->>>>>>> 75454ec51df02d66358c14789e13b992b1c17658
     </div>
   );
 }
