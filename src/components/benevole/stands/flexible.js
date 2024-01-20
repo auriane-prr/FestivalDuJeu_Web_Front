cef81f6d79544e5c325d92a4355aa6d7f0c34f82
MargauxThirion <thirion.margaux@gmail.com>
2024-01-20 19:20:31 +0100

    flexible

--- src/components/benevole/stands/flexible.js
+++ src/components/benevole/stands/flexible.js
@@ -3,8 +3,9 @@ import "../../../styles/benevoles/flexible.css";
 import Modal from "../../general/fenetre_modale";
 import Champ from "../../general/champ";
 import RadioButton from "../../general/radioButton";
+import Button from "../../general/bouton";
 
-function Flexible({ benevoleId }) {
+function Flexible() {
     const [modalOpen, setModalOpen] = useState(false);
     const [stands, setStands] = useState([]);
     const [dateDebut, setDateDebut] = useState("");
@@ -16,6 +17,34 @@ function Flexible({ benevoleId }) {
     const [successMessage, setSuccessMessage] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
     const [selectedStand, setSelectedStand] = useState("");
+    const [userId, setUserId] = useState('');
+    const[flexibleInfo, setFlexibleInfo] = useState('');
+    const [selections, setSelections] = useState([]);
+
+    const fetchUserId = async () => {
+        try {
+          const token = localStorage.getItem('authToken');
+          const pseudo = localStorage.getItem('pseudo');
+          const response = await fetch(`http://localhost:3500/benevole/pseudo/${pseudo}`, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+              }
+            });
+            if (response.ok) {
+              const { benevole } = await response.json();
+              console.log('benevoleId', benevole._id);
+              setUserId(benevole._id);
+            } else {
+              throw new Error('Erreur lors de la récupération des informations utilisateur');
+            }
+        } catch (error) {
+          console.error('Erreur lors de la récupération des informations utilisateur :', error);
+        }
+      };
+
+      
 
     useEffect(() => {
         // Exemple d'appel API pour récupérer les données du festival
@@ -29,13 +58,10 @@ function Flexible({ benevoleId }) {
             console.log(body.date_debut, body.date_fin);
         };
         fetchData();
+        fetchUserId();
       }, []);
 
       const handleOpenModal = () => setModalOpen(true);
-
-      const [horairesData, setHorairesData] = useState([
-        {date: selectedDate, heure: selectedHeure, liste_stand: selectedStand},
-      ]);
 
       function formatDate(date) {
         if (!date) return '';
@@ -64,24 +90,22 @@ function Flexible({ benevoleId }) {
                 heure: selectedHeure,
                 liste_stand: [{ id_stand: selectedStand }]
             }];
-        } catch (error) {
-            console.error("Erreur de connexion au serveur", error);
-        }
-        try {
+
             // Créez des objets flexible selon la sélection
             const flexibles = selectedDate === "both" ? 
             [{
-                benevole_id: benevoleId,
-                horaires: horairesData
+                benevole_id: userId,
+                horaire: [{ ...uptatedHorairesData[0], date: dateDebut }]
             }, {
-                benevole_id: benevoleId,
-                horaires: horairesData
+                benevole_id: userId,
+                horaire: [{ ...uptatedHorairesData[0], date: dateFin }]
             }] : [{
-                benevole_id: benevoleId,
-                horaires: horairesData
+                benevole_id: userId,
+                horaire: uptatedHorairesData
             }];
 
             for (let flexible of flexibles) {
+                console.log(flexible);
                 const response = await fetch(`http://localhost:3500/flexible`, {
                     method: "POST",
                     headers: {
@@ -93,7 +117,7 @@ function Flexible({ benevoleId }) {
                     throw new Error("Erreur lors de la création du flexible");
                 }
             }
-            setSuccessMessage("Votre choix flexible a été enregistré avec succès !");
+            setSuccessMessage("Votre flexibilité a été enregistré avec succès !");
             setErrorMessage("");
         } catch (error) {
             setErrorMessage("Erreur de connexion au serveur: " + error.message);
@@ -131,14 +155,83 @@ function Flexible({ benevoleId }) {
     const handleDateChange = (e) => {
         setSelectedDate(e.target.value);
     };
+    const handleAddStand = (date, heure, standId) => {
+        setSelections(prevSelections => {
+            const existingHoraireIndex = prevSelections.findIndex(h => h.date === date && h.heure === heure);
+            if (existingHoraireIndex >= 0) {
+                // Horaire existant, ajouter le stand à la liste
+                const newSelections = [...prevSelections];
+                newSelections[existingHoraireIndex].liste_stand.push({ id_stand: standId });
+                return newSelections;
+            } else {
+                // Nouvel horaire
+                return [...prevSelections, { date, heure, liste_stand: [{ id_stand: standId }] }];
+            }
+        });
+    };
+    
+
+    async function fetchFlexibleData() {
+        try {
+            const response = await fetch(`http://localhost:3500/flexible/benevole/${userId}`, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" },
+            });
+            if (response.ok) {
+                const flexibles = await response.json();
+                const horaires = flexibles.flatMap((flexible) => flexible.horaire);
+                const stands = horaires.flatMap((horaire) => horaire.liste_stand.map(stand => ({
+                    id: stand._id, 
+                    nom_stand: stand.nom_stand, 
+                    date: formatDate(horaire.date), 
+                    heure: horaire.heure
+                })));
+                setFlexibleInfo(stands);
+            } else {
+                console.error("Erreur lors de la récupération des stands");
+            }
+        } catch (error) {
+            console.error("Erreur de connexion au serveur", error);
+        }
+    }
+    useEffect(() => {
+        if (userId) {
+            fetchFlexibleData();
+        }
+    }, [userId]);
 
 
     
     return (
         <div className="button-flexible">
-            <button onClick={handleOpenModal}>Choisissez vos stands flexibles</button>
+            <button onClick={handleOpenModal}> Devenir flexible</button>
             {modalOpen && (
-                <Modal onClose={() => setModalOpen(false)}>
+                <Modal onClose={() => setModalOpen(false)} valeurDuTitre={"Choisissez vos stands flexibles"}>
+                    <div>
+                        <p className="inscription">Voici vos inscriptions :</p>
+                        {flexibleInfo.length > 0 ? (
+                            <ul>
+                                {flexibleInfo.map((stand, index) => (
+                                    <li key={index}>
+                                        Nom du stand: {stand.nom_stand}, Date:{stand.date}, Horaire: {stand.heure}
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>Vous n'êtes inscrit à aucun stand.</p>
+                        )}
+                    </div>
+                    <div>
+                        {selections.map((sel, index) => (
+                            <div key={index}>
+                                <p>Date: {sel.date}, Heure: {sel.heure}</p>
+                                <ul>
+                                    {sel.liste_stand.map((stand, idx) => <li key={idx}>{stand.id_stand}</li>)}
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
+
                     <RadioButton
                         options={radioOptions}
                         name="dateSelection"
@@ -154,8 +247,7 @@ function Flexible({ benevoleId }) {
                         />
                     </Champ>
                     <form onSubmit={handleSubmit}>
-                        <h2>Choisissez vos stands flexibles</h2>
-                        <p>{successMessage || errorMessage}</p>
+                        <p className="inscription">{successMessage || errorMessage}</p>
                         <Champ>
                             <select
                                 onChange={(e) => setSelectedStand(e.target.value)}
@@ -172,7 +264,7 @@ function Flexible({ benevoleId }) {
                                 ))}
                             </select>
                         </Champ>
-                        <button type="submit">Soumettre</button>
+                        <Button type="submit">Soumettre</Button>
                     </form>
                 </Modal>
             )}
