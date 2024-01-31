62350d142bd81df6e05a6994ad22bac9b949238f
MargauxThirion <thirion.margaux@gmail.com>
2024-01-31 20:03:16 +0100

    planning + flexible

---
+++ src/components/admin/flexible/flexible.js
@@ -0,0 +1,216 @@
+import React, { useState, useEffect } from 'react';
+import '../../../styles/Admin/flexible/flexible.css';
+import Bouton from '../../general/bouton';
+import Titre from '../../general/titre';
+
+function Flexible({ date }) {
+    const [flexibles, setFlexibles] = useState([]);
+    const [selectedFlexibleId, setSelectedFlexibleId] = useState(null);
+    const [selectedFlexible, setSelectedFlexible] = useState(null);
+    const [selectedStands, setSelectedStands] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // Récupérez la liste des bénévoles flexibles au chargement du composant
+  useEffect(() => {
+    fetch("http://localhost:3500/flexible")
+      .then(response => response.json())
+      .then(data => setFlexibles(data))
+      .catch(error => console.error("Erreur lors de la récupération des bénévoles flexibles: ", error));
+  }, []);
+
+  // Gestionnaire de clic pour afficher les informations du bénévole flexible sélectionné
+  const handleFlexibleClick = (flexibleId) => {
+    console.log("je suis dans handleFlexibleClick : ", flexibleId)
+    
+    if (selectedFlexibleId === flexibleId) {
+        setSelectedFlexibleId(null);
+        setSelectedFlexible(null);
+        return;
+      } 
+    setSelectedStands({}); 
+    console.log(`Bénévole flexible sélectionné: ${flexibleId}`);
+
+    // Récupérez les informations du bénévole flexible en fonction de son ID
+    fetch(`http://localhost:3500/flexible/benevole/${flexibleId}`)
+      .then(response => response.json())
+      .then(data => {
+        setSelectedFlexible(data);
+        console.log("information du bénévole flexible: ", data); 
+        console.log("pseudo", data.benevole_id[0].pseudo )
+      })
+      .catch(error => console.error("Erreur lors de la récupération des informations du bénévole flexible: ", error));
+      setSelectedFlexibleId(flexibleId);
+    
+  };
+  const fetchStandDetails = async (standId, horaire) => {
+    try {
+      const response = await fetch(`http://localhost:3500/stands/${standId}`);
+      const standData = await response.json();
+      if (!response.ok) throw new Error(standData.message || "Erreur lors de la récupération des détails du stand");
+
+      // Trouver les informations de cota pour l'horaire spécifique
+      const horaireCota = standData.horaireCota.find(h => h.heure === horaire);
+      if (!horaireCota) throw new Error("Horaire spécifique non trouvé dans le stand");
+
+      console.log(`Nombre de bénévoles pour le stand ${standId} à l'horaire ${horaire}: ${horaireCota.nb_benevole}`);
+      // Vous pouvez ici mettre à jour l'état pour afficher cette information dans l'UI si nécessaire
+    } catch (error) {
+      console.error("Erreur lors de la récupération des détails du stand: ", error.message);
+    }
+  };
+
+
+  const horaires = ["9-11", "11-14", "14-17", "17-20", "20-22"];
+
+  // Gestionnaire de clic pour les boutons de stand
+  const handleStandClick = (horaire, standId) => {
+    if (!selectedStands[horaire]) {
+      // Si aucun bouton n'a été sélectionné dans cet horaire, ajoutez le bouton
+      setSelectedStands({
+        ...selectedStands,
+        [horaire]: standId
+      });
+    } else {
+      // Si un bouton a déjà été sélectionné dans cet horaire, affichez un message d'erreur ou ne faites rien
+      console.log("Vous ne pouvez sélectionner qu'un stand par horaire.");
+    }
+    console.log("Stand sélectionné: ", standId, "Horaire: ", horaire);
+    fetchStandDetails(standId, horaire);
+  };
+
+  const handleSubmit = async () => {
+    if (Object.keys(selectedStands).length === 0) {
+      console.log("Aucun stand sélectionné.");
+      return;
+    }
+  
+    // Créez un tableau des stands sélectionnés
+    const selectedStandArray = Object.entries(selectedStands).map(([horaire, standId]) => ({
+      horaire,
+      standId,
+      idBenevole: selectedFlexibleId,
+    }));
+  
+    try {
+      for (const standInfo of selectedStandArray) {
+        console.log("standInfo :", standInfo.standId, standInfo.horaire, standInfo.idBenevole)
+        console.log("Types - standId:", typeof standInfo.standId, ", horaire:", typeof standInfo.horaire, ", idBenevole:", typeof standInfo.idBenevole);
+        const response = await fetch(`http://localhost:3500/stands/inscrire/${standInfo.standId}/${standInfo.horaire}/${standInfo.idBenevole}`, 
+        {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+  
+        if (!response.ok) {
+          throw new Error("Erreur lors de l'inscription à un stand.");
+        }
+      }
+  
+      console.log("Inscription réussie.");
+      setIsSubmitting(true);
+  
+      // Suppression du flexible après l'inscription réussie à tous les stands
+      const deleteResponse = await fetch(`http://localhost:3500/flexible/${selectedFlexibleId}`, {
+        method: 'DELETE',
+      });
+  
+      if (!deleteResponse.ok) {
+        throw new Error("Erreur lors de la suppression du flexible.");
+      }
+  
+      console.log("Flexible supprimé avec succès.");
+      setFlexibles(flexibles.filter(flexible => flexible.benevole_id[0]._id !== selectedFlexibleId));
+      setSelectedFlexibleId(null);
+      setSelectedFlexible(null);
+  
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  
+  
+
+  return (
+    <div>
+      <Titre valeurDuTitre={`Liste des bénévoles flexibles`} />
+        {flexibles.map((flexible,index) => (
+            <React.Fragment key={flexible._id}>
+                <div 
+                onClick={() => handleFlexibleClick(flexible.benevole_id[0]?._id)}
+                className={`flexible-item 
+                    ${selectedFlexibleId === flexible.benevole_id[0]?._id ? "selected" : ""
+                }`}
+                >
+                    <Titre valeurDuTitre={flexible.benevole_id[0]?.pseudo} />
+                </div>
+                {flexibles.length !== index && (
+                    <hr className="flexible-separator" />
+                )}
+            </React.Fragment>
+        ))}
+
+      {selectedFlexible && (
+        <div>
+          <Titre valeurDuTitre={`Choisissez les stands que vous voulez attribuer à ${selectedFlexible.benevole_id[0].pseudo}`}/>
+          {horaires.map((horaire, index) => (
+            <div key={index} className="flexible-row">
+              <div className="flexible-time">
+                {horaire}
+              </div>
+              <div className="flexible-stand">
+                {selectedFlexible.horaire && selectedFlexible.horaire.length > 0 ? (
+                  selectedFlexible.horaire
+                    .filter((horaireData) => horaireData.date === date && horaireData.heure === horaire)
+                    .map((horaireData) => (
+                      <div key={horaireData._id}>
+                        {horaireData.liste_stand && horaireData.liste_stand.length > 0 ? (
+                          horaireData.liste_stand.map((stand) => (
+                            <button className='button-stand'
+                              key={stand._id}
+                              onClick={() => handleStandClick(horaire, stand._id)} // Utilisez le gestionnaire de clic mis à jour
+                              style={{ borderColor: selectedStands[horaire] === stand._id ? '#454C8B' : 'rgba(170, 173, 191, 0.5)' }}
+                            >
+                              {stand.nom_stand}
+                            </button>
+                          ))
+                        ) : (
+                          <div>Aucun stand disponible</div>
+                        )}
+                      </div>
+                    ))
+                ) : (
+                  <div>Aucun horaire disponible</div>
+                )}
+              </div>
+            </div>
+          ))}
+          {Object.keys(selectedStands).length > 0 && (
+        <div>
+            <h3>Informations des stands sélectionnés</h3>
+            <ul>
+                {Object.keys(selectedStands).map((horaire, index) => (
+                    <li key={index}>
+                    Horaire: {horaire}, Stand: {selectedStands[horaire]}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )}
+    {!isSubmitting && (
+        <Bouton onClick={handleSubmit}>Soumettre</Bouton>
+    )}
+    {isSubmitting && (
+        <p>En cours de soumission...</p>
+    )}
+        </div>
+      )}
+
+    
+    </div>
+  );
+}
+
+export default Flexible;
